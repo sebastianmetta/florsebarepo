03c8a645709973ef834610b3acd16e6297637c89
Sebastian Metta (e21386) <Sebastian.Metta@cmegroup.com>
2014-01-29 16:40:56 -0300

    Modifying Junit

--- src/test/java/com/viridian/florsebaproject/AppTest.java
+++ src/test/java/com/viridian/florsebaproject/AppTest.java
@@ -2,13 +2,14 @@ package com.viridian.florsebaproject;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals("hola".length(), 4);
 	}
 
 }
