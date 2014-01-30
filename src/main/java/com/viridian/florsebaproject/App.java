cad1d82323c43f2931a745b3bbf22c5880b07a73
Florencia Tristant <flotristant@gmail.com>
2014-01-30 16:51:47 -0300

    Adding a test to read and write bytes

--- src/main/java/com/viridian/florsebaproject/App.java
+++ src/main/java/com/viridian/florsebaproject/App.java
@@ -1,9 +1,54 @@
 package com.viridian.florsebaproject;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class App {
 	
-	public static void main() {
-		System.out.println("Hola!!");
+	public static void main(String args[]) throws IOException {
+		readWriteTest();
+	}
+
+	private static void readWriteTest() throws IOException {
+		FileInputStream fin = null;
+		FileOutputStream fout = null;
+		try {
+			//read and write file
+			ByteBuffer buffer;
+			fin = new FileInputStream( "music.rar" );
+			//reading channel
+			FileChannel fc = fin.getChannel();
+			
+			fout = new FileOutputStream( "musicCopy.rar" );
+			// writing channel
+			FileChannel fcOut = fout.getChannel();
+			
+			buffer = ByteBuffer.allocate(2048);
+			while(true) {
+				buffer.clear();
+				if(fc.read( buffer ) == -1) {
+					break;
+				}
+				//reset the index (position, limit and capacity)
+				buffer.flip();
+				fcOut.write(buffer);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(fin != null) {
+				fin.close();
+			}
+			if(fout != null) {
+				fout.close();
+			}
+		}
 	}
 
 }
